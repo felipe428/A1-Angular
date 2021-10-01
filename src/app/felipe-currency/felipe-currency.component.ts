d250324502978467f7494df30e516e9ed1463d3f
felipe428 <felipe.silva428@fatec.sp.gov.br>
2021-10-01 12:13:37 -0300

    currency-done

--- src/app/felipe-currency/felipe-currency.component.ts
+++ src/app/felipe-currency/felipe-currency.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FelipeWalletService } from '../felipe-wallet.service';
 
 @Component({
   selector: 'app-felipe-currency',
   templateUrl: './felipe-currency.component.html',
-  styleUrls: ['./felipe-currency.component.css']
+  styleUrls: ['./felipe-currency.component.css'],
 })
 export class FelipeCurrencyComponent implements OnInit {
-
-  constructor() { }
+  constructor(public wallet: FelipeWalletService) {}
 
   ngOnInit() {
+    this.wallet.updateUSDeBRL();
+    this.wallet.updateEUR();
+    this.wallet.interval();
   }
 
+  updateUSDeBRL() {
+    this.wallet.updateUSDeBRL;
+  }
+
+  updateEUR() {
+    this.wallet.updateEUR();
+  }
+
+  interval() {
+    this.wallet.interval();
+  }
 }
--- src/app/felipe-wallet.service.ts
+++ src/app/felipe-wallet.service.ts
@@ -1,6 +1,52 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface USDeBRL {
+  bpi: {
+    USD: {
+      rate_float: number;
+    };
+    BRL: {
+      rate_float: number;
+    };
+  };
+}
+
+interface EUR {
+  bpi: {
+    EUR: {
+      rate_float: number;
+    };
+  };
+}
 
 @Injectable()
 export class FelipeWalletService {
-  constructor() {}
+  USDeBRLlist: Array<USDeBRL> = [];
+  EURlist: Array<EUR> = [];
+
+  constructor(private http: HttpClient) {}
+
+  updateUSDeBRL() {
+    this.http
+      .get<USDeBRL>('https://api.coindesk.com/v1/bpi/currentprice/BRL.json')
+      .subscribe((data) => {
+        this.USDeBRLlist.push(data);
+      });
+  }
+
+  updateEUR() {
+    this.http
+      .get<EUR>('https://api.coindesk.com/v1/bpi/currentprice/EUR.json')
+      .subscribe((data) => {
+        this.EURlist.push(data);
+      });
+  }
+
+  interval() {
+    setInterval(() => {
+      this.updateUSDeBRL();
+      this.updateEUR();
+    }, 60000);
+  }
 }
