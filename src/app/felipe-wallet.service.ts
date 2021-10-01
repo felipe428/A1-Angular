38f618a1bdc1797df8dd496921e6126a4df4d766
felipe428 <felipe.silva428@fatec.sp.gov.br>
2021-10-01 12:33:47 -0300

    done

--- src/app/felipe-wallet.service.ts
+++ src/app/felipe-wallet.service.ts
@@ -25,6 +25,8 @@ export class FelipeWalletService {
   USDeBRLlist: Array<USDeBRL> = [];
   EURlist: Array<EUR> = [];
 
+  saldo = 0;
+
   constructor(private http: HttpClient) {}
 
   updateUSDeBRL() {
@@ -48,5 +50,52 @@ export class FelipeWalletService {
       this.updateUSDeBRL();
       this.updateEUR();
     }, 60000);
+  }
+
+  somar(valor: number) {
+    if (valor > 0) {
+      this.saldo += valor;
+    }
+  }
+
+  subtrair(valor: number) {
+    if (valor <= this.saldo) {
+      this.saldo -= valor;
+    }
+  }
+
+  getSaldo() {
+    return this.saldo;
+  }
+
+  getDolar() {
+    if (this.USDeBRLlist.length > 0) {
+      return (
+        this.getSaldo() *
+        this.USDeBRLlist[this.USDeBRLlist.length - 1].bpi.USD.rate_float
+      ).toFixed(2);
+    }
+  }
+
+  getReal() {
+    if (this.USDeBRLlist.length > 0) {
+      return (
+        this.getSaldo() *
+        this.USDeBRLlist[this.USDeBRLlist.length - 1].bpi.BRL.rate_float
+      ).toFixed(2);
+    }
+  }
+
+  getEuro() {
+    if (this.EURlist.length > 0) {
+      return (
+        this.getSaldo() *
+        this.EURlist[this.EURlist.length - 1].bpi.EUR.rate_float
+      ).toFixed(2);
+    }
+  }
+
+  getBitcoin() {
+    return this.USDeBRLlist[this.USDeBRLlist.length - 1].bpi.BRL.rate_float;
   }
 }
--- src/app/felipe-wallet/felipe-wallet.component.ts
+++ src/app/felipe-wallet/felipe-wallet.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FelipeWalletService } from '../felipe-wallet.service';
 
 @Component({
   selector: 'app-felipe-wallet',
   templateUrl: './felipe-wallet.component.html',
-  styleUrls: ['./felipe-wallet.component.css']
+  styleUrls: ['./felipe-wallet.component.css'],
 })
 export class FelipeWalletComponent implements OnInit {
+  @Input() valor: number;
 
-  constructor() { }
+  constructor(public wallet: FelipeWalletService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  somar(valor: string) {
+    let calculo: number;
+    calculo = parseFloat(valor) / this.wallet.getBitcoin();
+    this.wallet.somar(calculo);
   }
 
+  subtrair(valor: string) {
+    let calculo: number;
+    calculo = parseFloat(valor) / this.wallet.getBitcoin();
+    this.wallet.subtrair(calculo);
+  }
 }
